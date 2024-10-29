a9ff036869e1a1758689d523a6e446b9bb129fad
danisaurrr <deicertheracer@gmail.com>
2024-10-29 20:11:24 +0800

    Animations for Warden in Progress
    Committing for the sake na di magka merge prob atm just so I could pull rawr

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [Header("Animator")]
+    public Animator anim;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        anim = GetComponent<Animator>();
     }
 
 
@@ -24,5 +27,11 @@ public class PlayerController : MonoBehaviour
         float y = 0;
         float z = Input.GetAxis("Vertical");
         gameObject.transform.Translate(x * speed * Time.deltaTime, y, z * speed * Time.deltaTime);
+
+        if (speed >= 5) {
+            anim.SetBool("isWalking", true);
+        } else {
+            anim.SetBool("isWalking", false);
+        }
     }
 }
