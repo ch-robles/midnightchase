afe5593ebaade0446f46d0751f0e92a6ec25161e
ch-robles <christine.robles.cics@ust.edu.ph>
2024-10-29 20:08:35 +0800

    Map fix for prims + Win Panel fix
    yes

---
+++ Assets/Scripts/MapFollowCharacter.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapFollowCharacter : MonoBehaviour
+{
+	public Transform characterReference;
+	public float characterOffset = 100f;
+
+	private void Update()
+	{
+		if(characterReference != null)
+		{
+			transform.position = new Vector3(characterReference.position.x, characterReference.position.y + characterOffset, characterReference.position.z);
+		}
+	}
+}
---
+++ Assets/Scripts/WinPanel.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinPanel : MonoBehaviour
+{
+    public GameObject winUI;
+    public GameObject inGameUI;
+
+    public bool playerWin = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerWin == true){
+            winUI.SetActive(true);
+            inGameUI.SetActive(false);
+            Manager.instance.Win();
+            playerWin = false;
+        }
+    }
+}
