e92723d212890511ac4a91be3e12e40a831a80b8
megubits <raeneelisha.quingquing.cics@ust.edu.ph>
2024-11-02 16:44:29 +0800

    enemy activate time extended
    considered for the countdown sa beginning too

--- Assets/EnemyActivator.cs
+++ Assets/EnemyActivator.cs
@@ -14,27 +14,27 @@ public class EnemyActivator : MonoBehaviour
     IEnumerator EnemyCountdown(){
         Debug.Log("Started level at timestamp: " + Time.time);
         
-        int seconds = 5;
+        int seconds = 10;
 
         if (Manager.instance.mazeSize == 20){
 
             // yield return new WaitForSeconds(5);
             // Debug.Log("Current mazesize is: " + Manager.instance.mazeSize);
-            seconds = 5;
+            seconds = 10;
 
         } else if (Manager.instance.mazeSize == 35){
             // yield return new WaitForSeconds(10);
             // Debug.Log("Current mazesize is: " + Manager.instance.mazeSize);
-            seconds = 10;
+            seconds = 15;
 
         } else if (Manager.instance.mazeSize == 50){
 
             // yield return new WaitForSeconds(20);
             // Debug.Log("Current mazesize is: " + Manager.instance.mazeSize);
-            seconds = 20;
+            seconds = 25;
         }
 
-        yield return new WaitForSeconds(seconds);
+        yield return new WaitForSeconds(seconds + 3);
 
         enemyObject.SetActive(true);
         Debug.Log("Deployed enemy at timestamp: " + Time.time);
